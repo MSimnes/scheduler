f32c72897d9b433ba057982be6983a1696966a45
Mike <mike.simnes@gmail.com>
2023-05-23 16:59:57 -0700

    add stories

--- stories/index.js
+++ stories/index.js
@@ -2,13 +2,24 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 import "index.scss";
+
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
+
+// Button
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -25,6 +36,8 @@ storiesOf("Button", module)
     </Button>
   ));
 
+
+// DayListItem  
 storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({  // Provides the default background color for our component
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -54,16 +67,119 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
     },
   ];
   
+
+  //DayList
   storiesOf("DayList", module)
     .addParameters({
       backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
     })
     .add("Monday", () => (
-      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+      <DayList days={days} day={"Monday"} onChange={action("setDay")} />
     ))
     .add("Tuesday", () => (
-      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+      <DayList days={days} day={"Tuesday"} onChange={action("setDay")} />
     ))
     .add("Wednesday", () => (
-        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+        <DayList days={days} day={"Wednesday"} onChange={action("setDay")} />
     ));
+
+
+//InterviewerListItem
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
+
+
+  //InterviewerList
+  const interviewers = [
+    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+  ];
+  
+  storiesOf("InterviewerList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Initial", () => (
+      <InterviewerList
+        interviewers={interviewers}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerList
+        interviewers={interviewers}
+        value={3}
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerList
+        interviewers={interviewers}
+        onChange={action("setInterviewer")}
+      />
+    ));
+
+
+    // Appointments
+storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds:[{name:"$white", value: "#fff", default: true}]
+    })
+    .add('Appointment', () => <Appointment />)
+    .add("Appointment with time", () => <Appointment time="12pm" />)
+    .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => (
+      <Show
+        student={'Lydia Miller-Jones'}
+        interviewer={'Sylvia Palmer'}
+        onEdit={action("onEdit")}  
+        onDelete={action("onDelete")}
+      />
+    ))
+    .add("Confirm", () => (
+      <Confirm 
+      message={"Delete the appointment?"}
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+      />
+    ))
+    .add("Status", () => <Status message="Deleting" />)
+    .add("Error", () => (
+      <Error 
+        message="Could not delete appoinment"
+        onClose={action('onClose')}
+      />
+    ))
