c17699b3c1e3bd6c81465086434849030230b538
Mike <mike.simnes@gmail.com>
2023-05-23 16:58:10 -0700

    create component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+import React, { useState } from 'react';
+import 'components/InterviewerListItem.scss';
+
+export default function InterviewerListItem(props) {
+  // const [interviewer, setInterviewer] = useState(props.selected)
+
+  const interviewerClass = classNames("interviewers__item", {
+    'interviewers__item--selected': props.selected,
+  })
+  const interviewerImageClass = classNames("interviewers__item-image", {
+    'interviewers__item-image--selected': props.selected,
+  })
+
+  return (
+    <li onClick={props.setInterviewer} className={interviewerClass}>
+      <img
+        className={interviewerImageClass}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && <>{props.name}</>}
+    </li>
+  )
+}
