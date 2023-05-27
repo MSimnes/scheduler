aedea5fe65c4a2721fa43fafb4db9c20bae2368e
Mike <mike.simnes@gmail.com>
2023-05-27 14:48:01 -0700

    import fragment and Appointment

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -17,7 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
-
+import Form from "components/Appointment/Form";
 
 // Button
 storiesOf("Button", module)
@@ -183,3 +183,39 @@ storiesOf("Appointment", module)
         onClose={action('onClose')}
       />
     ))
+    .add("Edit", () => (
+      <Form 
+        student={"Lydia Miller-Jones"}
+        interviewer={3}
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+        />))
+        .add('Create', () => (
+          <Form 
+          interviewers={interviewers}
+          onSave={action("onSave")}
+          onCancel={action("onCancel")}
+      />
+    ))
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="4pm" />
+        <Header time="5pm" />
+
+        {/* <Appointment time="5pm" /> */}
+      </Fragment>
+    ))
+    .add("Appointment Booked", () => (
+      <Fragment>
+        <Appointment
+          id={1}
+          time="4pm"
+          interview={{ student: "Lydia Miller-Jones", interviewer }}
+        />
+    <Header time='5pm' />
+
+        {/* <Appointment time="5pm" /> */}
+      </Fragment>
+    ))
+    
