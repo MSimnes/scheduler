69d6c9a52a38f8b686f3c17514e278f22db0b4a8
Mike <mike.simnes@gmail.com>
2023-05-29 16:58:04 -0700

    create getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,3 +15,14 @@ export function getAppointmentsForDay (state, day) {
 
   return result;
 }
+
+export function getInterview (state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const result = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  }
+  return result;
+}
