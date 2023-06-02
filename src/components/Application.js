fadd560fcd0e4643d26d2c0371912850f397741e
Mike <mike.simnes@gmail.com>
2023-06-02 15:15:48 -0700

    create function and make axios.put

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,46 +5,9 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
+import { getInterviewersForDay } from "helpers/selectors";
 import { getInterview } from "helpers/selectors";
 
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
 
 
 export default function Application(props) {
@@ -57,12 +20,43 @@ export default function Application(props) {
 
   const setDay = day => setState(prevState => ({ ...prevState, day }));
   // const setDays = days => setState(prev => ({ ...prev, days }));
-  
+
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   // create schedule object
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
+
+    //updates the state without mutating the original
+    //updates the local state with the new appointment object
+    //makes an axios request to update the database
+
+    function bookInterview(id, interview) {
+      const appointment = {
+        ...state.appointments[id],
+        interview: { ...interview }
+      };
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+      setState({ ...state, appointments });
+      //make the request to the correct endpoint with id and interview object
+      //upon response update the state with setState and the new appointments object
+      //spread the previous state and add the new appointments object
+      //transition to the SHOW mode
+      
+      axios.put(`/api/appointments/${id}`, { interview })
+        .then((response) => {
+          console.log("response:", response)
+          setState(prev => ({ ...prev, appointments }))
+        })
+
+    }
+
+
+
 
     return (
       <Appointment
@@ -70,6 +64,8 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        bookInterview={bookInterview}
+        interviewers={interviewers}
       />
     );
   });
@@ -81,7 +77,7 @@ export default function Application(props) {
       axios.get('/api/interviewers')
     ])
       .then((all) => {
-        console.log("all:", all)
+        console.log("all:", all[2].data)
         setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
       })
       .catch((error) => console.log("error:", error))
