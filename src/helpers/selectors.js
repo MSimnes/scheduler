f303892693ea89f8441ff4de257c346c24ee4a18
Mike <mike.simnes@gmail.com>
2023-06-02 15:18:06 -0700

    create function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,6 +16,24 @@ export function getAppointmentsForDay (state, day) {
   return result;
 }
 
+export function getInterviewersForDay (state, day) {
+  const result = [];
+  // retrieve the corresponding days object from the state object for the given day
+  const filteredDays = state.days.filter((item) => item.name === day);
+  if (filteredDays.length === 0) {
+    return result;
+  }
+  // access the appointments array and
+  // use the values as keys to retrieve the corresponding appointment objects
+  if (filteredDays.length > 0) {
+    for (let item of filteredDays[0].interviewers) {
+      result.push(state.interviewers[item])
+    }
+  }
+
+  return result;
+}
+
 export function getInterview (state, interview) {
   if (!interview) {
     return null;
