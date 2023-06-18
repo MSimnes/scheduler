4755d62872a64e62067541d370690d58dae41fdb
Mike <mike.simnes@gmail.com>
2023-06-17 18:49:32 -0700

    Clean up code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,46 +1,43 @@
-export function getAppointmentsForDay (state, day) {
+export function getAppointmentsForDay(state, day) {
   const result = [];
-  // retrieve the corresponding days object from the state object for the given day
   const filteredDays = state.days.filter((item) => item.name === day);
   if (filteredDays.length === 0) {
     return result;
   }
-  // access the appointments array and 
-  // use the values as keys to retrieve the corresponding appointment objects
+
   if (filteredDays.length > 0) {
     for (let item of filteredDays[0].appointments) {
-      result.push(state.appointments[item])
+      result.push(state.appointments[item]);
     }
   }
 
   return result;
 }
 
-export function getInterviewersForDay (state, day) {
+export function getInterviewersForDay(state, day) {
   const result = [];
-  // retrieve the corresponding days object from the state object for the given day
+
   const filteredDays = state.days.filter((item) => item.name === day);
   if (filteredDays.length === 0) {
     return result;
   }
-  // access the appointments array and
-  // use the values as keys to retrieve the corresponding appointment objects
+
   if (filteredDays.length > 0) {
     for (let item of filteredDays[0].interviewers) {
-      result.push(state.interviewers[item])
+      result.push(state.interviewers[item]);
     }
   }
 
   return result;
 }
 
-export function getInterview (state, interview) {
+export function getInterview(state, interview) {
   if (!interview) {
     return null;
   }
   const result = {
     student: interview.student,
-    interviewer: state.interviewers[interview.interviewer]
-  }
+    interviewer: state.interviewers[interview.interviewer],
+  };
   return result;
 }
