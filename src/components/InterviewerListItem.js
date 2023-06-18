7943ff4b92d9209430f8849ca2ce525aad56bbbe
Mike <mike.simnes@gmail.com>
2023-06-17 17:47:16 -0700

    Clean up code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,14 @@
-import classNames from 'classnames';
-import React from 'react';
-import 'components/InterviewerListItem.scss';
+import classNames from "classnames";
+import React from "react";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
   const interviewerClass = classNames("interviewers__item", {
-    'interviewers__item--selected': props.selected,
-  })
+    "interviewers__item--selected": props.selected,
+  });
   const interviewerImageClass = classNames("interviewers__item-image", {
-    'interviewers__item-image--selected': props.selected,
-  })
-
+    "interviewers__item-image--selected": props.selected,
+  });
   return (
     <li onClick={props.setInterviewer} className={interviewerClass}>
       <img
@@ -19,5 +18,5 @@ export default function InterviewerListItem(props) {
       />
       {props.selected && <>{props.name}</>}
     </li>
-  )
+  );
 }
