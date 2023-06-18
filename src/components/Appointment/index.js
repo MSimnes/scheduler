c29505de622dccee19cd4fe37b31e2a733f9b87c
Mike <mike.simnes@gmail.com>
2023-06-17 17:47:56 -0700

    add interviewer prop

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -73,6 +73,7 @@ export default function Appointment(props) {
         {mode === EDIT && (
           <Form
             interviewers={props.interviewers}
+            interviewer={props.interview.interviewer.id}
             student={props.interview.student}
             onCancel={() => back()}
             onSave={save}
