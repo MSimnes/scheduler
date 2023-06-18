229e00cf5d0eb7e7e1388d06eb595ab6c036cb64
Mike <mike.simnes@gmail.com>
2023-06-17 18:53:34 -0700

    Prettier

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,38 +1,33 @@
 import { useState } from "react";
 
-export default function useVisualMode (initial) {
-  const [mode, setMode] =useState(initial);
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition (mode, replace = false) {
+  function transition(mode, replace = false) {
     if (replace) {
-      setHistory(prev => {
+      setHistory((prev) => {
         const newHistory = [...prev.slice(0, prev.length - 1), mode];
         setMode(mode);
         return newHistory;
       });
     } else {
       setMode(mode);
-      setHistory(prev => [...prev, mode]);
+      setHistory((prev) => [...prev, mode]);
     }
-
-
   }
 
   function back() {
     setMode(initial);
-    setHistory(prev => {
+    setHistory((prev) => {
       if (prev.length > 1) {
         const newHistory = [...prev.slice(0, prev.length - 1)];
         setMode(newHistory[newHistory.length - 1]);
         return newHistory;
       }
       return prev;
-    }
-    );
+    });
   }
 
-  return {mode, transition, back};
+  return { mode, transition, back };
 }
-
-
